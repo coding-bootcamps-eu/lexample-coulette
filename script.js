14ccb029ba2db20d6d6146959f99a754e0422ab4
Nico Koenig <nico.koenig@fullstax.de>
2021-01-18 14:53:30 +0100

    toggle background color

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 console.log("welcome to coulette 🎨");
+
+/**
+ * Toggle color of header
+ */
+const button = document.querySelector("button");
+if (button) {
+  button.addEventListener("click", function () {
+    const color = "hotpink";
+    const header = document.querySelector("header");
+    const currentBackgroundColor = header.style.backgroundColor.toLowerCase();
+    if (currentBackgroundColor !== "hotpink") {
+      header.style.backgroundColor = color;
+    } else {
+      header.style.backgroundColor = null;
+    }
+  });
+} else {
+  console.error("button was not found.");
+}
